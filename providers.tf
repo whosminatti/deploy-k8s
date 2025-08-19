1c2102f046337ba2915363ccf95aec802fb368d8
Leandro Minatti de Souza <minatti.leandro@gmail.com>
2025-08-19 11:25:43 -0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
