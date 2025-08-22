db92419a59120994094f5eae40997494fa55cc80
Leandro Minatti de Souza <leandro.souza@cresol.com.br>
2025-08-22 16:39:21 -0300

    Cria helm setup e comenta terraform apply

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -17,3 +17,36 @@ output "node_iam_role_name" {
   description = "Nome do IAM Role dos nodes"
   value       = aws_iam_role.nodes.name
 }
+
+output "kubeconfig" {
+  description = "Kubeconfig"
+  value       = <<EOT
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${aws_eks_cluster.this.certificate_authority[0].data}
+    server: ${aws_eks_cluster.this.endpoint}
+  name: ${aws_eks_cluster.this.arn}
+contexts:
+- context:
+    cluster: ${aws_eks_cluster.this.arn}
+    user: ${aws_eks_cluster.this.arn}
+  name: ${aws_eks_cluster.this.arn}
+current-context: ${aws_eks_cluster.this.arn}
+kind: Config
+users:
+- name: ${aws_eks_cluster.this.arn}
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: aws
+      args:
+        - "eks"
+        - "get-token"
+        - "--cluster-name"
+        - "${aws_eks_cluster.this.name}"
+        - "--region"
+        - "us-east-1"
+EOT
+  sensitive = true
+}
---
+++ modules/helm/main.tf
@@ -0,0 +1,26 @@
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "4.0.13"
+  namespace  = "ingress-nginx"
+  create_namespace = true
+
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+}
+
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "metrics-server"
+  version    = "6.2.3"
+  namespace  = "kube-system"
+
+  set {
+    name  = "apiService.create"
+    value = "true"
+  }
+}
