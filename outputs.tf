551fb8ddd2e4bd32d681d7397cba48d934abc3ed
Leandro Minatti de Souza <leandro.souza@cresol.com.br>
2025-08-22 14:01:37 -0300

    Ajusta outputs do eks

--- outputs.tf
+++ outputs.tf
@@ -2,15 +2,11 @@ output "vpc_id" {
   description = "ID da VPC"
   value       = module.vpc.vpc_id
 }
-output "cluster_name" {
+output "project_name" {
   description = "Nome do cluster EKS"
-  value       = module.eks.cluster_name
+  value       = module.eks.project_name
 }
 output "cluster_endpoint" {
   description = "Endpoint do cluster EKS"
   value       = module.eks.cluster_endpoint
-}
-output "cluster_security_group_id" {
-  description = "Security Group do cluster"
-  value       = module.eks.cluster_security_group_id
 }
