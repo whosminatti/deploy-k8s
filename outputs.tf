0255ecca8185731b9a65af34ed247f4a8cc2f127
Leandro Minatti de Souza <leandro.souza@cresol.com.br>
2025-08-22 14:02:54 -0300

    remove project_name

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "vpc_id" {
   description = "ID da VPC"
   value       = module.vpc.vpc_id
 }
-output "project_name" {
-  description = "Nome do cluster EKS"
-  value       = module.eks.project_name
-}
 output "cluster_endpoint" {
   description = "Endpoint do cluster EKS"
   value       = module.eks.cluster_endpoint
